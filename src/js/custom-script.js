323a8ae30fda7000df22c0a5907a9dc7f4bbdbc6
mdio <michel.dio33@gmail.com>
2020-10-18 14:56:59 +0200

    navbar

---
+++ src/js/custom-script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+
+    /* click event sur le bouton du navbar */
+
+    // nav bar
+    $('.nav-button').click(function() {
+        $('.nav-button').toggleClass('change');
+    });
+    
+
+});
